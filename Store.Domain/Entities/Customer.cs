60c2766c8365de1f7feff8077659138df3080fae
Maximilliam Araujo <maximillian.alex@gmail.com>
2021-07-23 19:30:44 -0300

    Adicionando uma entidade base

--- Store.Domain/Entities/Customer.cs
+++ Store.Domain/Entities/Customer.cs
@@ -1,12 +1,14 @@
 namespace Store.Domain.Entities
 {
-    public class Customer{
+    public class Customer : Entity
+    {
       public Customer(string name, string email){
         Name = name;
         Email = email;
       }
       
       public string Name { get; private set; }
+
       public string Email { get; private set; }
     }
 }
---
+++ Store.Domain/Entities/Entity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Store.Domain.Entites 
+{
+    public class Entity
+    {
+        public Entity(){
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+    }
+}
