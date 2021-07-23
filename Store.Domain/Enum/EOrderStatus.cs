75e35a6302dfd8dd328afc1ff60bce409e625594
Maximilliam Araujo <maximillian.alex@gmail.com>
2021-07-23 20:25:49 -0300

    add enum para status do pedido

---
+++ Store.Domain/Enum/EOrderStatus.cs
@@ -0,0 +1,10 @@
+namespace Store.Domain.Enum
+{
+    public enum EOrderStatus
+    {
+        WaitingPaymant = 1,
+        WaitingDelivery = 2,
+        Canceled = 3,
+        Concluded = 4
+    }
+}
