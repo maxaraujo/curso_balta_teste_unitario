45b4a458a4ed5e89643ee693b241f3e9eca852f8
Maximilliam Araujo <maximillian.alex@gmail.com>
2021-07-24 12:28:01 -0300

    Add: test - status order must be canceled when it is cancel

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -39,5 +39,15 @@ namespace Store.Tests.Entities
 
             Assert.AreEqual(EOrderStatus.WaitingDelivery, order.Status);
         }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
+        {
+            var order = new Order(_customer, 0, null);
+            order.Cancel();
+
+            Assert.AreEqual(order.Status, EOrderStatus.Canceled);
+        }
     }
 }
