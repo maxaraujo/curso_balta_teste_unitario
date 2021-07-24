de21e94f4251afbf7adf006893a5bbf0d7b69423
Maximilliam Araujo <maximillian.alex@gmail.com>
2021-07-23 21:47:34 -0300

    make Entity inherit Notifiable of flunt

--- Store.Domain/Entities/Entity.cs
+++ Store.Domain/Entities/Entity.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
+using Flunt.Notifications;
 
 namespace Store.Domain.Entites 
 {
-    public class Entity
+    public class Entity : Notifiable
     {
         public Entity()
         {
             Id = Guid.NewGuid();
         }
         public Guid Id { get; private set; }
+
     }
 }
