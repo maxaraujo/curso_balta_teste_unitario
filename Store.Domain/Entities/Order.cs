feb25371b4aac98f97a0cfcd3147d7f0c52931a3
Maximilliam Araujo <maximillian.alex@gmail.com>
2021-07-23 21:08:22 -0300

    add entidade ordem do pedido

---
+++ Store.Domain/Entities/Order.cs
@@ -0,0 +1,48 @@
+
+namespace Store.Domain.Entities
+{
+    public class Order
+    {
+        public Order(Customer customer, decimal deliveryFee, Discount discount)
+        {
+            Customer = customer;
+            Date = DateTime.Now;
+            Number = Guid.NewGuid().ToString().Substring(0, 8);
+            DeliveryFee = deliveryFee;
+            Discount = discount;
+            Items = new List<OrderItem>();
+        }
+
+        public Customer Customer { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Number { get; private set; }
+        public IList<OrderItem> Items { get; private set; }
+
+        public decimal DeliveryFee { get; private set; }
+        public EOrderStatus Status { get; private set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            var item = new OrderItem(product, quantity);
+            Items.add(item);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach(var item in Items)
+            {
+                total += item.Total();
+            }
+            total += DeliveryFee;
+            total -= Discount != null ? Discount.Value() : 0;
+            return total;
+        }
+
+        public void Pay(decimal amount)
+        {
+            if(amount == Total())
+                this.Status = EOrderStatus.WaitingDelivery;
+        }
+    }
+}
